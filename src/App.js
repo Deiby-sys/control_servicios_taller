23f76fa0740896dc33fe2af6447bdb483dcef840
Deiby <deibyleandro@hotmail.com>
2025-04-28 21:16:04 -0500

    Add: form

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Control Servicios Taller</h1>
-
+      <h1>Control Servicios Taller</h1>
+      <form>
+            <label htmlFor="Usuario">Usuario: </label>       
+            <input type="email" id="Usuario" email="Usuario" placeholder="Email registrado"/>
+            <label htmlFor="Contraseña">Contraseña: </label>
+            <input type="password" id="Contraseña"/>
+            <input type="submit" class="btn" value="Ingresar"/>
+            <p><a Href="#">Recuperar contraseña</a></p>
+            <p><a Href="registro_usuario.html">Registro nuevo usuario</a></p>
+      </form>
         
     </div>
   );

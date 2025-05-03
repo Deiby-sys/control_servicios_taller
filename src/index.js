bde44d752727fc2e0e7c7e3461e5a2fe67f318a0
Deiby <deibyleandro@hotmail.com>
2025-05-02 22:21:07 -0500

    Add: pages

--- src/index.js
+++ src/index.js
@@ -3,38 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <div className="App">
-      <h1>Control Servicios Taller</h1>
-        <form>
-          <label htmlFor="Usuario">Usuario: </label>       
-          <input type="email" id="Usuario" email="Usuario" placeholder="Email registrado"/>
-          <label htmlFor="Contraseña">Contraseña: </label>
-          <input type="password" id="Contraseña"/>
-          <input type="submit" className="btn" value="Ingresar"/>
-          <a
-            className="link_Recuperar"
-            href="http"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Recuperar Contraseña
-          </a>
-          <br></br>
-          <a
-            className="link_Registro"
-            href="http"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Registro nuevo usuario
-          </a>
-            
-        </form>
-    </div>
-  );
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
---
+++ src/pages/registro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Registro() {
+
+    <div className="Registro">
+      <h1>Control Servicios Taller</h1>
+        <form>
+          
+            
+        </form>
+    </div>
+}

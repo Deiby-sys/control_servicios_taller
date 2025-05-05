ddd283c77529af6fceb56d39b52391b114981f55
Deiby <deibyleandro@hotmail.com>
2025-05-04 20:38:25 -0500

    Add: css

--- src/pages/RegistroUsuario.js
+++ src/pages/RegistroUsuario.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import './RegistroUsuario.css';
 
 export default function RegistroUsuario() {
 
   return (
     <div className="RegistroUsuario">
-      <h1>Control Servicios Taller</h1>
-      <pr></pr>
-      <h2>Registro Usuario</h2>
-        <form>
-          
-            
-        </form>
+      <form>
+        <h2>Registro Usuario</h2>
+        <input type="text" placeholder="Nombre"/>
+        <input type="text" placeholder="Apellido"/>
+        <input type="email" placeholder="Correo"/>
+        <select className="perfil"/>
+            <option>Selecciona tu perfil:</option>
+            <option>Asesor</option>
+            <option>Bodega</option>
+            <option>Jefe</option>
+            <option>Técnico</option>  
+        
+        <input type="password" placeholder="Contraseña"/>
+        <input type="password" placeholder="Confirma Contraseña"/>
+        <input type="submit" class="btn" value="Enviar"/>
+      </form>
     </div>
   );
 }

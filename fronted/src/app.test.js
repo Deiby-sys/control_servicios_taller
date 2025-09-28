cbc6890e81f79c7b90e0aea14c259e49e9e3b1b6
Deiby <deibyleandro@hotmail.com>
2025-09-27 20:04:43 -0500

    Login, registerUser Frontend

--- fronted/src/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import app from './App';
-
-test('renders learn react link', () => {
-  render(<app />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
